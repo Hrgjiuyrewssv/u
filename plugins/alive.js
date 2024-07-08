f3b7aa7f2565bb90ece105a3fc70b924733c8546
Hrgjiuyrewssv <babamon339@gmail.com>
2024-07-09 01:54:14 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -38,13 +38,12 @@ Badan({
 	fromMe: false,
 	desc: 'anti delete',
 	type: 'whatsapp'
-}, async (message, match, client) => {
+}, async (message) => {
 	if (ANTI_DELETE) {
-		let msg = await message.client.store.loadMessage(message.messageId)
+		let msg = await message.client.store.loadMessage(message.messageId);
 		let { pushName } = msg.message;
 		let name = pushName.trim().replace(/\s+/g, ' ');
-		//let name = "hi"
 		let sudo = numToJid(SUDO.split(',')[0]) || message.client.user.id;
-		await message.forwardMessage(sudo, msg.message , { contextInfo: { isFrowarded: false, externalAdReply: { title: "deleted message", body: `from: ${name}`, mediaType: 2, thumbnail: "https://banner2.cleanpng.com/20231217/wca/transparent-cartoon-garbage-overflow-trash-bin-waste-managemen-chaotic-overflowing-garbage-bin-with-assorted-1710959851836.webp", mediaUrl: "", sourceUrl: "" }}, quoted: msg.message })
+		await message.forwardMessage(sudo, msg.message , { contextInfo: { isFrowarded: false, externalAdReply: { title: "deleted message", body: `from: ${name}`, mediaType: 2, thumbnail: "https://i.imgur.com/xItorgn.jpeg", mediaUrl: "", sourceUrl: "" }}, quoted: msg.message })
 	}
 });
