b03757ddb972f607d96fb82318911a6240021181
Hrgjiuyrewssv <babamon339@gmail.com>
2024-11-28 12:18:18 +0530

    Update eval.js

--- plugins/eval.js
+++ plugins/eval.js
@@ -7,7 +7,7 @@ lib.Badan({ pattern:'eval', on: "text", fromMe: true, desc :'Runs a server code'
   if (message.text.startsWith(">")) {
     const m = message;
     try {
-      let evaled = await eval(`${message.text.replace(">", "")}`);
+      let evaled = await eval(`(async () => { ${message.text.replace(">", "")}})()`);
       if (typeof evaled !== "string") evaled = require("util").inspect(evaled);
       await message.reply(evaled);
     } catch (err) {
